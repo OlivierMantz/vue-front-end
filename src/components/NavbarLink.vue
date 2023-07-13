070dd982a49de255f1c85ec3a882b9a04cc1a100
OlivierMantz <OlivierMantz>
2023-07-13 11:16:24 +0200

    Added async calls to retrieve pages and saved theme in local storage

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <a
+        class="nav-link"
+        :class="activeClasses"
+        aria-current="page"
+        :href="page.link.url"
+        :title="`This is a link to the ${page.link.text} page`"
+        >{{ page.link.text }}</a
+    >
+</template>
+<script>
+export default {
+    props: ["page", "isActive"],
+    computed: {
+        activeClasses() {
+            return {
+                active: this.isActive,
+                emphasize: this.isActive,
+            };
+        },
+    },
+};
+</script>
+<style>
+.emphasize scoped{
+    text-decoration: underline !important;
+}
+</style>
